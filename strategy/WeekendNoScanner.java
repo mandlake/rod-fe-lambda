2341d5bca84e39b87524c47838762a0b2a7cd043
Sion Kim <94289633+mandlake@users.noreply.github.com>
2024-03-14 15:23:23 +0900

    make weekend

---
+++ strategy/WeekendNoScanner.java
@@ -0,0 +1,4 @@
+package com.rod.api.strategy;
+
+public class WeekendNoScanner {
+}
---
+++ strategy/WeekendStrategy.java
@@ -0,0 +1,35 @@
+package com.rod.api.strategy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public enum WeekendStrategy {
+    MONDAY("1", i  -> "Monday"),
+    TUESDAY("2", i  -> "Tuesday"),
+    WEDNESDAY("3", i  -> "Wednesday"),
+    THURSDAY("4", i  -> "Thursday"),
+    FRIDAY("5", i  -> "Friday"),
+    SATURDAY("6", i  -> "Saturday"),
+    SUNDAY("7", i  -> "Sunday"),
+    ERROR("", i -> "Wrong")
+    ;
+
+    private final String day;
+    private final Function<String, String> function;
+
+    WeekendStrategy(String day, Function<String, String> function) {
+        this.day = day;
+        this.function = function;
+    }
+
+    public static String execute(Scanner sc) {
+        System.out.println("1 ~ 7 입력: ");
+        String s = sc.next();
+        return Stream.of(values())
+                .filter(i -> i.day.equals(s))
+                .findAny()
+                .orElse(ERROR).function.apply(s);
+    }
+}
